e1199e1b736f75b16764601951b8101c72ac2fd2
Nadiia Khasyshyn <nadia.khasyshyn@gmail.com>
2025-06-14 15:01:47 +0300

    Add User and Expense entities

---
+++ src/main/java/com/example/finance_tracker/entities/User.java
@@ -0,0 +1,36 @@
+package com.example.finance_tracker.entities;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Expense> expenses;
+}
+
